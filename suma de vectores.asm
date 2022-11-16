51409e52f7d811057ba3a2686cf1e81e7a4c8904
Richar649 <116041114+Richar649@users.noreply.github.com>
2022-11-15 19:37:32 -0600

    Update suma de vectores.asm

--- suma de vectores.asm
+++ suma de vectores.asm
@@ -1,23 +1,23 @@
 .data
-        array1: .word 1 2 3 4 5    #declaracion de matriz1
-        array2: .word 6 7 8 9 10   #declaracion de matriz2
-        array3: .word 0 0 0 0 0    #declaracion de matriz3
+        array1: .word 1 2 3 4 5    #declaracion de matriz 1
+        array2: .word 6 7 8 9 10   #declaracion de matriz 2
+        array3: .word 0 0 0 0 0    #declaracion de matriz 3
         
 .text
 
 main:  
-        la $s1, array1   #matriz1
-        la $s2, array2   #matriz2
-        la $s3, array3   #matriz3
+        la $s1, array1   #matriz 1
+        la $s2, array2   #matriz 2
+        la $s3, array3   #matriz 3
        
 while:                   #declaracion de while
         slti $t1,$s3,5   
-        addi $t1,$s3,5   #sumamos la matriz mas5 
+        addi $t1,$s3,5   #sumamos la matriz mas 5 
         j while          #ejecutamos el while
  
-        lw  $t1,($s3)    #cargamos en t1 matriz3
-        lw  $t2,($s2)    #cargamos en t1 matriz2
-        lw  $t3,($s1)    #cargamos en t1 matriz1
+        lw  $t1,($s3)    #cargamos en t1 matriz 3
+        lw  $t2,($s2)    #cargamos en t2 matriz 2
+        lw  $t3,($s1)    #cargamos en t3 matriz 1
         add $t4,$t2,$t1  #sumamos las matrices
         sw  $t1,-100($t2)  #resultado 
         
